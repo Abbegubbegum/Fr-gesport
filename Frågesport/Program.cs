65fb37fffecac2f23bff42cd41c98fbcc2bdad81
Albin <albin.nojback@gmail.com>
2020-09-21 11:21:11 +0200

    Randomizing the positions of alterantives

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -10,34 +10,33 @@ namespace Frågesport
             string answer = "";
             bool spela = true;
 
+
             string q1 = "First question, How many bones in a shark?";
             string a11 = "250";
             string a12 = "100";
-            string a13 = "0";
-            string c1 = "3";
+            string a1c = "0";
 
             string q2 = "Second question, This one is a douzie! Var lägger gökar ägg?";
             string a21 = "I en grop i marken";
-            string a22 = "I andra fåglars bo";
-            string a23 = "I vanliga bon av pinnar";
-            string c2 = "2";
+            string a2c = "I andra fåglars bo";
+            string a22 = "I vanliga bon av pinnar";
+
 
             string q3 = "Third question, What is this planet called?";
-            string a31 = "Saturn";
+            string a3c = "Saturn";
             string a32 = "Neptune";
-            string a33 = "Uranus";
-            string c3 = "1";
+            string a31 = "Uranus";
 
             while (spela)
             {
                 points = 0;
                 System.Console.WriteLine("Welcome to the show! 1 right answer is 100 points, let's see who's a dumb boi!");
 
-                points += Fråga(q1, a11, a12, a13, c1);
+                points += Fråga(q1, a1c, a11, a12);
 
-                points += Fråga(q2, a21, a22, a23, c2);
+                points += Fråga(q2, a2c, a21, a22);
 
-                points += BFråga(q3, a31, a32, a33, c3);
+                points += BFråga(q3, a3c, a31, a32);
 
                 System.Console.WriteLine("Alright results are in!");
                 System.Console.WriteLine("You got: " + points + " points!");
@@ -73,20 +72,41 @@ namespace Frågesport
                 }
             }
         }
-        static int Fråga(string q, string a1, string a2, string a3, string c)
+        static int Fråga(string q, string ac, string a1, string a2)
         {
-            string ans = "";
-            while (ans != "1" && ans != "2" && ans != "3")
+            Random rnd = new Random();
+            int ans = 0;
+            int c = rnd.Next(0, 3);
+            int a = rnd.Next(0, 3);
+            string[] options = { "", "", "" };
+
+            options[c] = ac;
+
+            while (options[a] == ac)
+            {
+                a = rnd.Next(0, 3);
+            }
+            options[a] = a1;
+
+            a = rnd.Next(0, 3);
+
+            while (options[a] == ac || options[a] == a1)
+            {
+                a = rnd.Next(0, 3);
+            }
+            options[a] = a2;
+
+            while (ans != 1 && ans != 2 && ans != 3)
             {
                 System.Console.WriteLine(q);
-                System.Console.WriteLine("1: " + a1);
-                System.Console.WriteLine("2: " + a2);
-                System.Console.WriteLine("3: " + a3);
+                System.Console.WriteLine("1: " + options[0]);
+                System.Console.WriteLine("2: " + options[1]);
+                System.Console.WriteLine("3: " + options[2]);
 
-                ans = Console.ReadLine();
+                ans = Convert.ToInt32(Console.ReadLine());
             }
 
-            if (ans == c)
+            if (ans == c + 1)
             {
                 System.Console.WriteLine("Correct!");
                 return 100;
@@ -96,14 +116,31 @@ namespace Frågesport
                 System.Console.WriteLine("Fail!");
                 return 0;
             }
-
-
-
         }
-        static int BFråga(string q, string a1, string a2, string a3, string c)
+        static int BFråga(string q, string ac, string a1, string a2)
         {
-            string ans = "";
-            while (ans != "1" && ans != "2" && ans != "3")
+            Random rnd = new Random();
+            int ans = 0;
+            int c = rnd.Next(0, 3);
+            int a = rnd.Next(0, 3);
+            string[] options = { "", "", "" };
+
+            options[c] = ac;
+
+            while (options[a] == ac)
+            {
+                a = rnd.Next(0, 3);
+            }
+            options[a] = a1;
+
+            a = rnd.Next(0, 3);
+
+            while (options[a] == ac || options[a] == a1)
+            {
+                a = rnd.Next(0, 3);
+            }
+            options[a] = a2;
+            while (ans != 1 && ans != 2 && ans != 3)
             {
                 Console.WriteLine("    _...MMMMM88&&&&..._");
                 Console.WriteLine(" .::'''MMMMM88&&&&&&'''::.");
@@ -112,14 +149,14 @@ namespace Frågesport
                 Console.WriteLine("   `''''MMMMM88&&&&''''`");
                 Console.WriteLine("         'MMM8&&&'");
                 System.Console.WriteLine(q);
-                System.Console.WriteLine("1: " + a1);
-                System.Console.WriteLine("2: " + a2);
-                System.Console.WriteLine("3: " + a3);
+                System.Console.WriteLine("1: " + options[0]);
+                System.Console.WriteLine("2: " + options[1]);
+                System.Console.WriteLine("3: " + options[2]);
 
-                ans = Console.ReadLine();
+                ans = Convert.ToInt32(Console.ReadLine());
             }
 
-            if (ans == c)
+            if (ans == c + 1)
             {
                 System.Console.WriteLine("Correct!");
                 return 100;
